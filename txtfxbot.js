359550d6d12c7f8e980bf261d5544f27d44caebb
Evan <evan.straw99@gmail.com>
2017-01-19 13:43:23 -0800

    Added header to file

---
+++ txtfxbot.js
@@ -0,0 +1,8 @@
+/******************************************
+ *               TxtFX Bot                *
+ *         Created by Evan Straw          *
+ * Applies various Unicode pseudoalphabet *
+ * effects to text inputted via Telegram. *
+ ******************************************/
+
+ 
